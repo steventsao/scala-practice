22d1bdebe065a02c1266d63559f2f19c42af93e8
steven <steventsao713@gmail.com>
2017-07-05 23:10:08 -0700

    Add my own function

--- hw.scala
+++ hw.scala
@@ -1,8 +1,12 @@
+// $ sbt
+// $ > ~ run // to monitor source change
+
 object Coursera {
   def main(args: Array[String]) = {
-    println(s"Simple product is ${simpleProduct((x, y) => x * y)(7, 2)}")
-    println(s"Product is ${product(x => x*x)(4, 3)}")
-    println(s"Factorial is ${factorial(5)}")
+    println(stringToNum(x => s"$x is my age")(19))
+//    println(s"Simple product is ${simpleProduct((x, y) => x * y)(7, 2)}")
+//    println(s"Product is ${product(x => x*x)(3, 4)}")
+//    println(s"Factorial is ${factorial(5)}")
   }
   def simpleProduct(f: (Int, Int) => Int)(a: Int, b: Int): Int =
     // A trivial currying solution
@@ -13,4 +17,11 @@ object Coursera {
     else f(a) * product(f)(a + 1, b)
 
   def factorial(n: Int) = product(x => x)(1, n)
+  def mapReduce(f: Int => Int, combine: (Int, Int) => Int, zero: Int)(a: Int, b: Int): Int =
+    if (a > b) zero
+    else combine(f(a), mapReduce(f, combine, zero)(a+1, b))
+
+  // Currying evaluates from right to left
+  def stringToNum(f: Int => String)(a: Int): String =
+    if (a > 18) f(a) else "Not old enough"
 }
